bde3280b655573beba1b577b553a7f12785ab66e
cristoflop <cristofer.lc.99@gmail.com>
2021-10-08 14:30:26 +0200

    Add endpoint to get file content and updated docker stack

--- src/main/java/com/indra/hiperion/kafka/controllers/TopicUtilsApi.java
+++ src/main/java/com/indra/hiperion/kafka/controllers/TopicUtilsApi.java
@@ -4,6 +4,9 @@ package com.indra.hiperion.kafka.controllers;
 import com.indra.hiperion.kafka.services.dto.MessageDto;
 import com.indra.hiperion.kafka.services.dto.TopicInfoDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,12 +20,26 @@ public interface TopicUtilsApi {
     @GetMapping("/topics")
     Set<TopicInfoDto> getTopics();
 
-    @Operation(summary = "Produce un mensaje en un topico")
+    @Operation(summary = "Produce un mensaje en un topico",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = MessageDto.class),
+                            examples = {
+                                    @ExampleObject(value = "{\n" +
+                                            "    \"topic\": \"BCN_BUFFER_MP_GSI_RAW\",\n" +
+                                            "    \"message\": \"{ 'mensaje': 'mensaje de prueba' }\"\n" +
+                                            "}")
+                            })))
     @PostMapping("/topics")
     void addMessageInTopic(@RequestBody MessageDto messageDto);
 
     @Operation(summary = "Produce un mensaje en todos los topicos de patron BCN_*_JSON")
     @PatchMapping("/topics")
     void cleanUpTopics();
+
+    @Operation(summary = "Muestra el fichero de log con todos los mensajes consumidos desde que se arrancó el servicio")
+    @GetMapping("/topic")
+    String consumerMonitoring();
 
 }
